beb2116294f3bb33585d91227850f75589beb8f2
windowhan <windowhan@gmail.com>
2023-12-15 16:21:24 +0900

    remove useless comment
    remove useless comment

--- market-contracts/src/IntentMarket/AppLauncher.sol
+++ market-contracts/src/IntentMarket/AppLauncher.sol
@@ -22,7 +22,6 @@ contract AppLauncher {
         engine = OrderMatchEngine(_engine);
     }
 
-    // 파라미터에서 contraints랑 prevStateParseConfig 제거
     function run(uint256 intentID) public {
         require(engine.getWinner(intentID) == tx.origin, "not winner!");
 
@@ -39,15 +38,3 @@ contract AppLauncher {
         engine.setExecuted(intentID);
     }
 }
-
-/*
-일어나면 해야할거
-
-** 어떻게 AppLauncher의 run함수를 실행하는 payload를 generate하지?
--> UserOperation을 generate해서 아예 올려버리기. (nonce replace attack(?)이 가능할 수 있지만 일단 올려보자)
--> 나중에는 별도의 Validator를 만들어서 플레이해볼 수 있을 것
-
-1. AppLauncher에서 constraints랑 prevStateParseConfig 제거하기.
-2. AppLauncher에서 OrderMatch Winner만 접근 가능하게 만들기.
-3. 실행까지 완벽하게 하게 만들기
-*/
